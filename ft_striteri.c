dac9693f230745295202c6a9118c765dacb1bff1
Soufiane Essarhir <sofiane.essarhir@edu.uiz.ac.ma>
2023-11-05 07:46:38 +0100

    edited

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: sessarhi <sessarhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/05 06:52:10 by sessarhi          #+#    #+#             */
-/*   Updated: 2023/11/05 07:23:12 by sessarhi         ###   ########.fr       */
+/*   Updated: 2023/11/05 07:46:30 by sessarhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,3 +33,6 @@ void    ft_striteri(char *s, void (*f)(unsigned int,char*))
 //     ft_striteri(str, uppercase);
 // 	printf("%s\n", str);
 // }
+
+
+
